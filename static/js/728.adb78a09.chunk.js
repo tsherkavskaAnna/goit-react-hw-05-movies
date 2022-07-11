1aaacb848bbcc098882e6ceb790a9c0e3ad6bbeb
tsherkavskaAnna <80473872+tsherkavskaAnna@users.noreply.github.com>
2022-07-11 20:19:30 +0000

    Deploying to gh-pages from @ tsherkavskaAnna/goit-react-hw-05-movies@03b2465bc873503b14fa39d27ada9de4eec19baf 🚀

---
+++ static/js/728.adb78a09.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[728],{3728:function(t,e,a){a.d(e,{BYu:function(){return r}});var n=a(9983);function r(t){return(0,n.w_)({tag:"svg",attr:{version:"1",viewBox:"0 0 48 48",enableBackground:"new 0 0 48 48"},child:[{tag:"polygon",attr:{fill:"#3F51B5",points:"4,19 18,30.7 18,7.3"}},{tag:"path",attr:{fill:"#3F51B5",d:"M42,27v13h-8V27c0-2.2-1.8-4-4-4H13v-8h17C36.6,15,42,20.4,42,27z"}}]})(t)}}}]);
+//# sourceMappingURL=728.adb78a09.chunk.js.map
